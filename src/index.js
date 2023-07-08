9919794f753857d7ee6dc23c1b72ec243d60387e
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-07-08 13:11:42 +0900

    const

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import { createStore } from "redux";
 
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
